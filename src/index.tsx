d87b0dfcbd1b32ba8fe0a1a6a342e6bb3de0b028
Gustavo Bispo <38411510+gustavolbs@users.noreply.github.com>
2022-11-10 10:44:27 -0300

    test: add mirage server for testing purposes

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,16 @@ import type { ReactElement } from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { makeServer } from './server';
 import Theme from './theme';
 
 import Routes from './routes';
 
 import './styles.css';
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer({ environment: 'development' });
+}
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = ReactDOM.createRoot(document.getElementById('root')!);
