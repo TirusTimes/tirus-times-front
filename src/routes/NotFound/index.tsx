f3d05e369eabed91da0d5166ec5c83a09f0d8e1c
Vinicius Lacerda <jose.arruda@profusion.mobi>
2022-11-13 17:45:51 -0300

    feat: create NotFound route component

---
+++ src/routes/NotFound/index.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+import { Stack } from '@mui/system';
+
+const NotFound = (): JSX.Element => {
+  return (
+    <Stack alignItems="center" justifyItems="center">
+      <Typography variant="h1">Page not found</Typography>
+    </Stack>
+  );
+};
+
+export default NotFound;
